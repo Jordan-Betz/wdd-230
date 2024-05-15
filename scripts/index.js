b0b07296e8e4e83593d93c58d7c2178176fb45ab
Jordan-Betz <jordan.betz@yahoo.com>
2024-05-14 22:33:58 -0600

    Home page changes

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,16 @@
-console.log("Hello, World!")
+// Hamburer navigation
+const hamburgerNav = document.getElementById('nav-toggle');
+const navigation = document.querySelector('.nav-list');
+
+hamburgerNav.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamburgerNav.classList.toggle('open');
+})
+
+// Darkmode
+const darkmodeButton = document.getElementById('darkmode-toggle');
+const body = document.querySelector('body');
+
+darkmodeButton.addEventListener('click', () => {
+    body.classList.toggle('dark');
+})
