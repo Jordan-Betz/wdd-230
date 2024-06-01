6c9f0a12ddd7eae334ef098a6b99fa2a54aaa856
Jordan-Betz <jordan.betz@yahoo.com>
2024-05-31 18:32:05 -0600

    W03 Team Activity - Home Page Responsive Images

--- scripts/index.js
+++ scripts/index.js
@@ -14,3 +14,19 @@ const body = document.querySelector('body');
 darkmodeButton.addEventListener('click', () => {
     body.classList.toggle('dark');
 })
+
+// Visit Count
+const visitDisplay = document.getElementById('visit-count');
+let numberVisits = Number(window.localStorage.getItem('numberVisits-ls')) || 0;
+
+console.log(numberVisits)
+
+if (numberVisits !== 0) {
+    visitDisplay.textContent = numberVisits;
+} else {
+    visitDisplay.textContent = 'This is your first visit. Welcome!';
+}
+
+numberVisits++;
+
+localStorage.setItem('numberVisits-ls', numberVisits);
