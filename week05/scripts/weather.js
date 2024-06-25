5b4bbd1edb16744365d34269aa21d8f23c9f1526
Jordan-Betz <147632554+Jordan-Betz@users.noreply.github.com>
2024-06-25 10:49:19 -0600

    Week 5 Weather API Learning Activity

---
+++ week05/scripts/weather.js
@@ -0,0 +1,38 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+let latitude = 49.7476
+let longitude = 6.6405
+
+const appID = 'b5ae5a43e286fc2ff3ff0ff1bd0357d7'
+
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${appID}&units=imperial`
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); // testing only
+        displayResults(data); // uncomment when ready
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+apiFetch();
+
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', 'Weather icon');
+    captionDesc.textContent = `${desc}`;
+}
